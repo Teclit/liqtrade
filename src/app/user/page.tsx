2d9d84633f8eec7c6188716ceb37cd6dcd9e7e42
Teclit <teklitte@gmail.com>
2025-02-23 10:58:35 +0100

    feat(sidebar)update sidebar menu height

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,7 +3,7 @@ import Sidebar from "@/components/Sidebar";
 
 export default function UserDashboard() {
     return (
-        <div className="flex min-h-screen bg-gray-100">
+        <div className="flex h-auto bg-gray-100">
             {/* Sidebar */}
             <Sidebar/>
 
